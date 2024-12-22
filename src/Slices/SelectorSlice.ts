716c1d9931504da839db59c31866000784aad8aa
Hal Hawkins <hal.hawkins@gmail.com>
2024-12-21 21:43:47 -0500

    Prevent different weather layers from being selected at the same time.

--- src/Slices/SelectorSlice.ts
+++ src/Slices/SelectorSlice.ts
@@ -4,16 +4,27 @@ const initialState = {
   layers: [] as string[], // Example: Array of layer names
 };
 
+const distinctLayers = [
+  "clouds",
+  "temperature",
+  "precipitation",
+  "pressure",
+  "wind",
+  // "stations",
+];
+
 export const selectorSlice = createSlice({
   name: "selector",
   initialState,
   reducers: {
     toggleLayer: (state, action) => {
-      const index = state.layers.indexOf(action.payload);
-      if (index > -1) {
-        state.layers.splice(index, 1); // Remove layer if it exists
+      const selectedLayer = action.payload;
+      if (state.layers.includes(selectedLayer)) {
+        // Remove the layer if it exists
+        state.layers = state.layers.filter(layer => layer !== selectedLayer);
       } else {
-        state.layers.push(action.payload); // Add layer if it doesn't exist
+        // Remove all other layers and add the selected layer
+        state.layers = [selectedLayer];
       }
     },
   },
